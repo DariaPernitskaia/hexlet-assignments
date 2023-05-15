35955800c557954e7e20ca5b8a900a91d9819920
@hexlet/cli <support@hexlet.io>
2023-05-15 16:48:10 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -8,19 +8,28 @@ public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordCount = new HashMap<>();
 
+        // Разбиваем предложение на слова
         String[] words = sentence.split(" ");
+
+        // Считаем количество повторений каждого слова
         for (String word : words) {
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            if (!word.isEmpty()) {
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
         }
+
         return wordCount;
     }
 
     public static String toString(Map<String, Integer> wordCount) {
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
+
+        // Обходим все ключи словаря
         for (String word : wordCount.keySet()) {
             sb.append("  ").append(word).append(": ").append(wordCount.get(word)).append("\n");
         }
+
         sb.append("}");
 
         return sb.toString();
